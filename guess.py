2f6a08624850758ca6840593a1771def9e1cfd28
rithwik777222 <rithwikk312@gmail.com>
2025-03-28 21:44:59 +0530

    guess

---
+++ guess.py
@@ -0,0 +1,10 @@
+x = [6,7,8,9,10,11,12,13,14,15]
+
+i = int(input("number whose triple is less than an eleventh of its cube by 88:"))
+
+if i == 11:
+    print("your answer is right")
+elif i < 11:
+    print("your answer is lower than the requred soltion")
+else :
+    print("your number is greaer than the required solution")
